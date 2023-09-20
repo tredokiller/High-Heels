2326af28d9f6bae89a9e0b9dac4c51c2e3e7a7b6
foxstudent103020 <vlad.tredo86@gmail.com>
2023-09-20 20:52:56 +0300

    Added Heels Spawn Mechanic

--- Assets/Player/Scripts/HeelsSpawner.cs
+++ Assets/Player/Scripts/HeelsSpawner.cs
@@ -1,14 +1,12 @@
-using System;
 using System.Collections.Generic;
-using Common.Enums;
 using UnityEngine;
 
 namespace Player.Scripts
 {
     public class HeelsSpawner : MonoBehaviour
     {
-        private const float VerticalDistanceBetweenHeels = 1.6f;
-        
+        private const float DistanceBetweenHeels = 1.5f;
+
         [SerializeField] private Transform leftFoot;
         [SerializeField] private Transform rightFoot;
 
@@ -22,29 +20,41 @@ namespace Player.Scripts
         private void Start()
         {
             _heels = new List<Heels.Scripts.Heels>();
-            SpawnHeels();
         }
 
         private void Update()
         {
             leftFootTransformTranslator.position = leftFoot.position;
             rightFootTransformTranslator.position = rightFoot.position;
-            if (_heels.Count >0)
-            {
-                for (int i = 0; i < _heels.Count; i++)
-                {
-                    _heels[i].SetPosition(Directions.Left , leftFoot.position + Vector3.down * (VerticalDistanceBetweenHeels * i));
-                    _heels[i].SetPosition(Directions.Right , rightFoot.position + Vector3.down * (VerticalDistanceBetweenHeels * i));
-                }
-            }
+
+            leftFootTransformTranslator.localRotation = Quaternion.Euler(leftFoot.localRotation.eulerAngles.x, 0, leftFoot.localRotation.eulerAngles.z - 3);
+            rightFootTransformTranslator.localRotation = Quaternion.Euler(rightFoot.localRotation.eulerAngles.x, 0, rightFoot.localRotation.eulerAngles.z + 3);
         }
 
         public void SpawnHeels()
         {
             var heelsPref = Instantiate(heelsPrefab, transform);
             var heels = heelsPref.GetComponent<Heels.Scripts.Heels>();
-            _heels.Add(heels);
             
+            var leftHeel = heels.GetLeftHeel();
+            var rightHeel = heels.GetRightHeel();
+            
+            leftHeel.SetParent(leftFootTransformTranslator); 
+            leftHeel.localRotation = Quaternion.Euler(Vector3.zero);
+            
+            rightHeel.SetParent(rightFootTransformTranslator);
+            rightHeel.localRotation = Quaternion.Euler(Vector3.zero);
+            
+            var heelsSpawnPosition = Vector3.down *_heels.Count * DistanceBetweenHeels;
+            if (_heels.Count == 0)
+            {
+                heelsSpawnPosition = Vector3.down * 1 * (DistanceBetweenHeels / 2);
+            }
+            
+            leftHeel.localPosition = heelsSpawnPosition;
+            rightHeel.localPosition = heelsSpawnPosition;
+            
+            _heels.Add(heels);
         }
     }
 }
